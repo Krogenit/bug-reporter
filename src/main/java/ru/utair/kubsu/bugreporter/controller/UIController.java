6a8867739debbcf7b3ec92bf70b86b19f0c86c5b
Tkachenko_IG <tenkrg@gmail.com>
2018-07-23 16:40:10 +0300

    post requsts, photos, full bug ready

--- src/main/java/ru/utair/kubsu/bugreporter/controller/UIController.java
+++ src/main/java/ru/utair/kubsu/bugreporter/controller/UIController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import ru.utair.kubsu.bugreporter.Bug;
 
+import java.io.File;
 import java.util.List;
 
 @RestController
@@ -18,59 +19,66 @@ public class UIController {
     @Autowired
     private RestTemplate rest;
 
+    JiraClient jira;
+
     @PostMapping(path = "add_bug")
     public String createIssue(@RequestBody Bug bug) {
-        HttpEntity<Bug> request = new HttpEntity<>(bug);
-        String url = "https://jira.utair.ru/rest/api/2/issue";
-        ResponseEntity<String> response = rest.exchange(url, HttpMethod.POST, request, String.class);
-        //com.atlassian.
 
-        BasicCredentials creds =
-                new BasicCredentials("Krogenit", "davikdavik123");
-        JiraClient jira = new JiraClient("jira.utair.ru", creds);
-
-        try {
-            Issue newIssue = jira.createIssue("TEST", "Bug")
-                    .field(Field.SUMMARY, "Bat signal is broken")
-                    .field(Field.DESCRIPTION, "Commissioner Gordon reports the Bat signal is broken.")
-                    .field(Field.REPORTER, "batman")
-                    .field(Field.ASSIGNEE, "robin")
-                    .execute();
-        } catch (JiraException e) {
-            System.err.println(e.getMessage());
-            if (e.getCause() != null)
-                System.err.println(e.getCause().getMessage());
-           // e.printStackTrace();
+        if(jira == null) {
+            BasicCredentials creds =
+                    new BasicCredentials("Krogenit", "davikdavik123");
+            jira = new JiraClient("https://jira.utair.ru", creds);
         }
 
-        return response.getStatusCode().toString();
+        try {
+            Issue newIssue = jira.createIssue(bug.getProject(), "Ошибка")
+                    .field(Field.SUMMARY, bug.getSummary())
+                    .field(Field.DESCRIPTION, bug.getDescription() + "\n" + bug.getMail() +
+                    "\n" + bug.getName())
+                    //.field(Field.REPORTER, "Krogenit")
+                    .field(Field.ASSIGNEE, "Krogenit")
+                    .field(Field.PRIORITY, bug.getPriority())
+//                    .field(Field.DUE_DATE, bug.getDate())
+                    .execute();
+//            newIssue.addAttachment();
+            File f = new File(".", "test.png");
+            newIssue.addAttachment(f);
+            //System.out.println(f.getAbsolutePath());
+        } catch (JiraException e) {
+            e.printStackTrace();
+        }
+
+        return "kek";
     }
 
     @GetMapping(path = "bug")
     public String createIssue() {
-        BasicCredentials creds =
-                new BasicCredentials("Krogenit", "davikdavik123");
-        JiraClient jira = new JiraClient("jira.utair.ru", creds);
 
-        /*try {
-            Issue newIssue = jira.createIssue("TEST", "Bug")
-                    .field(Field.SUMMARY, "Bat signal is broken")
-                    .field(Field.DESCRIPTION, "Commissioner Gordon reports the Bat signal is broken.")
-                    .field(Field.REPORTER, "batman")
-                    .field(Field.ASSIGNEE, "robin")
+        if(jira == null) {
+            BasicCredentials creds =
+                    new BasicCredentials("Krogenit", "davikdavik123");
+            jira = new JiraClient("https://jira.utair.ru", creds);
+        }
+
+        try {
+            Issue newIssue = jira.createIssue("KUBPRCTC", "Ошибка")
+                    .field(Field.SUMMARY, "короче давид тут такое дело читай внутри")
+                    .field(Field.DESCRIPTION, "предлагаю бахнуть по шаве после спринта тупа атпразднывать и в манйкрафт завалица.")
+                    //.field(Field.REPORTER, "Krogenit")
+                    .field(Field.ASSIGNEE, "Krogenit")
                     .execute();
         } catch (JiraException e) {
             e.printStackTrace();
-        }*/
+        }
 
-        try {
+        /*try {
             List<Project> projects = jira.getProjects();
             for(Project p : projects) {
                 System.out.println(p.getName());
             }
         } catch (JiraException e) {
             e.printStackTrace();
-        }
+        }*/
 
         return "ok";
     }}
